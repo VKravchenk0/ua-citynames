88932c7c6085d2bd7c84d758501461a5b0ee2bdd
Viacheslav Kravchenko <vk@consulting.mk>
2021-09-01 02:54:13 +0300

    Resetting output directory

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import pandas as pd
 import re
+import os
+import shutil
+from pathlib import Path
 
 dev = True
 # dev = False
+
+base_output_dir = './output'
 
 settlement_types_dict = {
     'С': 'с. ',
@@ -10,6 +15,27 @@ settlement_types_dict = {
     'Т': 'смт. ',
     'М': 'м. '
 }
+
+
+def get_output_file_name():
+    filename = 'result_dev.txt' if dev else 'result.txt'
+    return f"{base_output_dir}/{filename}"
+
+
+def reset_output_directory():
+    delete_output_dir_if_exists()
+    create_output_dir()
+
+
+def delete_output_dir_if_exists():
+    dirpath = Path(base_output_dir)
+    if dirpath.exists() and dirpath.is_dir():
+        shutil.rmtree(dirpath)
+
+
+def create_output_dir():
+    filename = get_output_file_name()
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
 
 
 def print_df(step_name):
@@ -41,6 +67,8 @@ def process_case(name):
 def add_settlement_type_prefix(row):
     return settlement_types_dict[row['category']] + row['display_name']
 
+
+reset_output_directory()
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', 1000)
@@ -87,15 +115,14 @@ item_counts = df["display_name"].value_counts()[df['display_name'].unique()]
 # item_counts = df["display_name"].value_counts() # - сортування за кількістю населених пунктів з назвою
 print(item_counts)
 
-if not dev:
-    longest_string_length = df.display_name.str.len().max()
-    number_of_dots = longest_string_length + 10
-    with open('result.txt', 'w') as fp:
-        count_label = "Кількість"
-        count_label_length = len(count_label)
-        fp.write(f'{"Назва":.<{number_of_dots - count_label_length}}{count_label}\n')
-        for idx, name in enumerate(item_counts.index.tolist()):
-            count = item_counts[idx]
-            # for formatting purposes
-            number_of_digits_in_count = len(str(count))
-            fp.write(f'{name:.<{number_of_dots - number_of_digits_in_count}}{count}\n')
+longest_string_length = df.display_name.str.len().max()
+number_of_dots = longest_string_length + 10
+with open(get_output_file_name(), 'w') as fp:
+    count_label = "Кількість"
+    count_label_length = len(count_label)
+    fp.write(f'{"Назва":.<{number_of_dots - count_label_length}}{count_label}\n')
+    for idx, name in enumerate(item_counts.index.tolist()):
+        count = item_counts[idx]
+        # for formatting purposes
+        number_of_digits_in_count = len(str(count))
+        fp.write(f'{name:.<{number_of_dots - number_of_digits_in_count}}{count}\n')
